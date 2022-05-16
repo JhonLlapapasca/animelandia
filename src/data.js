c1327205eefae8fd9a31bc8f6cdfc6f53456ebc7
Ronal James <92068615+Jamesllm@users.noreply.github.com>
2022-05-16 10:04:18 -0500

    add new data

---
+++ src/data.js
@@ -0,0 +1,182 @@
+let avances = [
+  {
+    id: 1004,
+    category: "serie",
+    title: "Kono Subarashii Sekai ni Shukufuku wo!",
+    background:
+      "https://www.themoviedb.org/t/p/original/bGKFAQsxS0dzRYSvyspRm0rZlfo.jpg",
+    trailer: "https://www.youtube.com/embed/5h4rQY9bpgE",
+  },
+];
+
+let recomendados = [
+  {
+    id: 1001,
+    category: "serie",
+    title: "Spy x Family",
+    information:
+      "Todo el mundo tiene una parte de sí mismos que no puede mostrar a los demás. En una era en la que las naciones de todo el mundo se encuentran involucradas en una feroz guerra de información a puerta cerrada, Ostania y Westalis llevan décadas en guerra fría. La División de Inteligencia de Westalis (WISE) envía a su mejor espía, Twilight, en una misión ultrasecreta para vigilar los movimientos de Donovan Desmond, quien dirige el Partido Nacional por la Unidad de Ostania, responsable de bombardear los esfuerzos de paz entre ambos países",
+    trailer: "https://www.youtube.com/embed/PyjSxCWvq5s",
+    background:
+      "https://www.themoviedb.org/t/p/original/wgMxlmsqonV8vymd0JimlUvg82D.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/original/vaIREdPGZOnW9Jrs86mheVVlIQQ.jpg",
+    episodes: [
+      {
+        id: 1,
+        src: "https://api.mycdn.moe/ext.php?id=16313",
+      },
+    ],
+  },
+  {
+    id: 1002,
+    title: "Tonikaku Kawaii",
+    category: "serie",
+    information:
+      "La comedia romántica sigue a un chico llamado Nasa quien conoce a la hermosa chica Tsukasa el día de su ingreso a la preparatoria, e inmediatamente se enamora de ella. Nasa le confiesa sus sentimientos, pero ella le responde que solo saldrá con él si se casan.Años después, cuando Nasa cumple 18 años, Tsukasa aparece de nuevo en su vida solicitándole el prometido matrimonio. Así comenzará su vida de recién casados.",
+    trailer: "https://www.youtube.com/embed/EYhWTsndKt4",
+    background:
+      "https://www.themoviedb.org/t/p/original/rnf2BDKeF1sxPEPhAcdvL0auuxr.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/original/jJKTrIfZKoFV66HGMzSa4tkObK0.jpg",
+    episodes: [
+      {
+        id: 1,
+        src: "https://api.mycdn.moe/sblink.php?id=dkyhfdn9uhj0",
+      },
+    ],
+  },
+  {
+    id: 1003,
+    category: "serie",
+    title: "Kobayashi san Chi no Maid Dragon",
+    information:
+      "Kobayashi vive sola en un apartamento, hasta que un día, Tooru aparece en su vida y terminan viviendo juntas. Tooru mira con desprecio a los seres humanos, los cuales, percibe como inferiores y simples, pero después de haber sido salvada por Kobayashi, ella hace todo lo posible para devolverle el favor y la ayuda en todo tipo de cosas, aunque no todo va de acuerdo al plan.",
+    trailer: "https://www.youtube.com/embed/MxMNAo8d17E",
+    background:
+      "https://www.themoviedb.org/t/p/original/vc3GVrH46tZPUk7RxRpQO6wH3ny.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/original/l1yFML11jzQqrKho371dSRwIQUI.jpg",
+    episodes: [
+      {
+        id: 1,
+        src: "https://api.mycdn.moe/sblink.php?id=zpy4c4s2b1ge",
+      },
+    ],
+  },
+  {
+    id: 1004,
+    category: "serie",
+    title: "Kono Subarashii Sekai ni Shukufuku wo",
+    information:
+      "La vida del amante de los videojuegos, Satou Kazuma, termina cuando es arrollado por un camión. En un inesperado giro del destino reencarna en otro mundo gracias a la diosa Aqua, a una torpe hechicera llamada Megumin y a una delirante caballero llamada Darkness. En esta película este peculiar grupo se enfrentará a una increíble amenaza. ¿Podrán salvar la Villa Carmesí? ¿Qué pasará con Kazuma?",
+    trailer: "https://www.youtube.com/embed/5h4rQY9bpgE",
+    background:
+      "https://www.themoviedb.org/t/p/original/bGKFAQsxS0dzRYSvyspRm0rZlfo.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/original/lnlEWSvfiHbQOgVyfnEqU1q31Yg.jpg",
+    episodes: [
+      { id: 1, src: "https://api.mycdn.moe/sblink.php?id=76m7lu9ph4ev" },
+    ],
+  },
+  {
+    id: 1005,
+    category: "serie",
+    title: "Re:Zero Empezar de cero en un mundo diferente",
+    information:
+      "En la historia, Subaru Natsuki es un estudiante corriente de instituto que se pierde en un mundo alternativo, donde una preciosa chica de pelo plateado lo rescata. Para devolverle el favor decide quedarse con ella, pero el destino con el que carga la muchacha es mucho más pesado de lo que Subaru puede imaginar. Los enemigos atacan sin descanso, uno tras otro, hasta que finalmente mueren tanto él como la chica. Es entonces cuando Subaru descubre que tiene el poder de dar marcha atrás en el tiempo y volver al inicio de la historia, al punto en el que llegó al mundo desconocido. El problema es que él es el único que recuerda lo ocurrido.",
+    trailer: "https://www.youtube.com/embed/XZiakX-wvnk",
+    background:
+      "https://www.themoviedb.org/t/p/original/e0770pcg8ZLR7sWnpiaOz4wlOco.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/original/t3lgcb3RHb2PKyHWf1hU6RQXH6N.jpg",
+    episodes: [
+      { id: 1, src: "https://api.mycdn.moe/sblink.php?id=qx0qz1ta44hg" },
+    ],
+  },
+  {
+    id: 1006,
+    category: "serie",
+    title: "Kaguya-sama wa Kokurasetai Tensai-tachi no Renai Zunousen",
+    information:
+      "¡Todo vale en el amor y en la guerra! Dos genios. Dos cerebros. Dos corazones. Una batalla. ¡¿Quién declarará primero su amor?! Kaguya Shinomiya y Miyuki Shirogane son dos prodigios de gran inteligencia y quienes controlan el consejo de estudiantes de su prestigiosa academia, lo que las convierte en la crème de la crème de la élite. Pero claro, estar en la cima es algo que incluye una buena dosis de soledad como extra, y ambos acaban enamorándose el uno del otro. ¿El problema? Que ambos son demasiado orgullosos como para admitir que están enamorados, así que el primero que lo admita, ¡será el perdedor! Comienza su batalla diaria para intentar forzar al otro a declararse primero.",
+    trailer: "https://www.youtube.com/embed/rZ95aZmQu_8",
+    background:
+      "https://www.themoviedb.org/t/p/original/qKh9R3tsSUA8tuwTlxvhoGz4jYd.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/original/osvx9K21S04gaoL1PNNv1ZXrvG.jpg",
+    episodes: [
+      { id: 1, src: "https://api.mycdn.moe/sblink.php?id=9brm5j78ubp3" },
+    ],
+  },
+];
+let movies = [
+  {
+    id: 1001,
+    category: "movie",
+    title: "Sonic: La película",
+    information:
+      "Sonic, el descarado erizo azul basado en la famosa serie de videojuegos de Sega, vivirá aventuras y desventuras cuando conoce a su amigo humano y policía, Tom Wachowski (James Marsden). Sonic y Tom unirán sus fuerzas para tratar de detener los planes del malvado Dr. Robotnik (Jim Carrey), que intenta atrapar a Sonic con el fin de emplear sus inmensos poderes para dominar el mundo.",
+    trailer: "https://www.youtube.com/embed/Xb3E8eWZ1mk",
+    background:
+      "https://www.themoviedb.org/t/p/original/4OCvIHWGIrWv030ykwOYS1Wz9Ur.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/original/rK25c71fYVi0Bv7RrTChK7NAQjC.jpg",
+    episodes: [
+      {
+        id: 1,
+        src: "https://api.mycdn.moe/video/tt3794354/",
+      },
+    ],
+  },
+  {
+    id: 1002,
+    category: "movie",
+    title: "En el bosque de la luz de las luciérnagas",
+    information:
+      "Cuando era pequeña, Hotaru se perdió en el bosque cerca del lugar donde va todos los veranos, la casa de su tío. Allí tuvo un extraño encuentro con Gin que la ayuda a encontrar el camino de vuelta. Gin es un joven que lleva una máscara blanca y que, debido a un hechizo, no puede ser tocado por ningún humano. Ella empieza a visitarlo todos los veranos según va creciendo, mientras espera la llegada del verano, para que su tiempo juntos continúe.",
+    trailer: "https://www.youtube.com/embed/VRvtWoI2RCU",
+    background:
+      "https://www.themoviedb.org/t/p/original/hAHF7nzJHUKwrhyl3nB5ckSLHuc.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/original/xE763r1dnYji0gOxrSxBtzyouSf.jpg",
+    episodes: [
+      { id: 1, src: "https://api.mycdn.moe/sblink.php?id=zw8gs1cdkd8u" },
+    ],
+  },
+  {
+    id: 1003,
+    category: "movie",
+    title: "Los Tipos Malos",
+    information:
+      "Cinco villanos notorios: el Sr. Wolf, Mr. Snake, Mr. Piranha, Mr. Shark y Ms. Tarantula, que han pasado toda una vida juntos realizando grandes atracos.",
+    trailer: "https://www.youtube.com/embed/qyYvtl6mttc",
+    background:
+      "https://www.themoviedb.org/t/p/original/fEe5fe82qHzjO4yej0o79etqsWV.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/original/wFdwJh3fbhp5aiRbQelVz1mbbwP.jpg",
+    episodes: [
+      {
+        id: 1,
+        src: "https://api.mycdn.moe/video/tt8115900/"
+      }
+    ],
+  },
+];
+
+export function getAvances() {
+  return avances;
+}
+export function getMovies() {
+  return movies;
+}
+export function getRecomendados() {
+  return recomendados;
+}
+
+export function getRecomendado(id) {
+  return recomendados.find((recomendado) => recomendado.id === id);
+}
+export function getMovie(id) {
+  return movies.find((movie) => movie.id === id);
+}
